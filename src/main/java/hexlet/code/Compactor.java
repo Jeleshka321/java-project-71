94f584b559becbf132dd8834f1ce404a1563c278
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-07-24 05:04:10 +0700

    Added Many things

--- src/main/java/hexlet/code/Compactor.java
+++ src/main/java/hexlet/code/Compactor.java
@@ -1,15 +1,44 @@
 package hexlet.code;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Compactor {
+    public static List<Map<String, Object>> compare(Map<String, Object> map1,
+                                                               Map<String, Object> map2) {
+        var difference = new ArrayList<Map<String, Object>>();
 
-    public static List<Map<String,Object>> compare(Map<String, Object> file1, Map<String,Object> file2) {
-        var keys = new TreeSet<>();
-        keys.addAll(file1.keySet());
-        keys.addAll(file2.keySet());
-        return List.of(Map.of());
+        var keys = new TreeSet<String>(map1.keySet());
+        keys.addAll(map2.keySet());
+
+        for (var key : keys) {
+            var infoAboutElement = new HashMap<String, Object>();
+
+            if (!map1.containsKey(key)) {
+                infoAboutElement.put("status", "added");
+                infoAboutElement.put("key", key);
+                infoAboutElement.put("value", map2.get(key));
+            } else if (!map2.containsKey(key)) {
+                infoAboutElement.put("status", "removed");
+                infoAboutElement.put("key", key);
+                infoAboutElement.put("value", map1.get(key));
+            } else if (Objects.equals(map1.get(key), map2.get(key))) {
+                infoAboutElement.put("status", "unchanged");
+                infoAboutElement.put("key", key);
+                infoAboutElement.put("value", map1.get(key));
+            } else {
+                infoAboutElement.put("status", "updated");
+                infoAboutElement.put("key", key);
+                infoAboutElement.put("oldValue", map1.get(key));
+                infoAboutElement.put("newValue", map2.get(key));
+            }
+
+            difference.add(infoAboutElement);
+        }
+        return difference;
     }
 }
