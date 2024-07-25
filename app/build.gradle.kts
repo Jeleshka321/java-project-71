3ec56d2478a57daf1259933177c3c2aa17cb62e8
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-07-26 05:13:18 +0700

    4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,46 +1,35 @@
-plugins {
-    id 'java'
-    id 'application'
-    id 'checkstyle'
-    id 'jacoco'
+    plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+    id("io.freefair.lombok") version "8.6"
 }
 
-group = 'hexlet.code'
-version = '1.0-SNAPSHOT'
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
 }
 
+jacoco {
+    toolVersion = "0.8.12"
+}
+
 dependencies {
-    testImplementation platform('org.junit:junit-bom:5.9.1')
-    testImplementation 'org.junit.jupiter:junit-jupiter'
-    testImplementation('org.assertj:assertj-core:3.22.0')
-    testImplementation('org.junit.jupiter:junit-jupiter-params:5.10.0-M1')
-
-    implementation 'info.picocli:picocli:4.7.3'
-    annotationProcessor 'info.picocli:picocli-codegen:4.7.3'
-
-    implementation 'com.fasterxml.jackson.core:jackson-databind:2.14.2'
-    implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2'
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation ("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
+    implementation("commons-io:commons-io:2.7")
 }
 
-test {
+tasks.test {
     useJUnitPlatform()
-    finalizedBy jacocoTestReport
-}
-
-application {
-    mainClass = 'hexlet.code.App'
-}
-
-compileJava {
-    options.release = 17
-    options.compilerArgs += ["-Aproject=${project.group}/${project.name}"]
-}
-
-jacocoTestReport {
-    reports {
-        xml.required = true
-    }
 }
