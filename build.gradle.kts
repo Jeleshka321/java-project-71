d12e3fd0eecb993f88d24031c700dda63679f780
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-07-26 04:42:02 +0700

    Added gradle

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+    id("io.freefair.lombok") version "8.6"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories {
+    mavenCentral()
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation ("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
+    implementation("commons-io:commons-io:2.7")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
