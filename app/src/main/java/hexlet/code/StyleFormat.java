13b601843e5a3fd8a3ce2ade81a337696890b596
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-07-26 05:04:44 +0700

    4

---
+++ app/src/main/java/hexlet/code/StyleFormat.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import hexlet.code.formatters.JsonFormatter;
+import hexlet.code.formatters.PlainFormatter;
+import hexlet.code.formatters.StylishFormatter;
+
+import java.util.List;
+import java.util.Map;
+
+public class StyleFormat {
+    // transforms the result into the required format
+    public static String format( List<Map<String, Object>> compareResult, String format)
+            throws JsonProcessingException {
+
+        return switch (format) {
+            case "stylish" -> StylishFormatter.format(compareResult);
+            case "plain" -> PlainFormatter.format(compareResult);
+            case "json" -> JsonFormatter.format(compareResult);
+            default -> throw new RuntimeException("Unknown format of output");
+        };
+    }
+
+}
