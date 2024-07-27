8784c06233c7c513ae78703df868e2d5843297cd
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-07-27 20:45:08 +0700

    Changed StyleFormat

--- app/src/main/java/hexlet/code/StyleFormat.java
+++ app/src/main/java/hexlet/code/StyleFormat.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import hexlet.code.formatters.JsonFormatter;
 import hexlet.code.formatters.PlainFormatter;
 import hexlet.code.formatters.StylishFormatter;
@@ -10,13 +10,12 @@ import java.util.Map;
 
 public class StyleFormat {
     // transforms the result into the required format
-    public static String format( List<Map<String, Object>> compareResult, String format) throws JsonProcessingException {
-
+    public static String format( List<Map<String, Object>> compareResult, String format)  {
         return switch (format) {
             case "stylish" -> StylishFormatter.format(compareResult);
             case "plain" -> PlainFormatter.format(compareResult);
             case "json" -> JsonFormatter.format(compareResult);
-            default -> throw new RuntimeException("Unknown format of output");
+            default -> throw new IllegalArgumentException("Unknown data extension:" + format);
         };
     }
 
