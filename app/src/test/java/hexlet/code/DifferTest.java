8d5b9feb92e72b23e61c23d1409282277f355431
Jeleshka321 <78028927+Jeleshka321@users.noreply.github.com>
2024-07-31 08:37:34 +0700

    Changed DifferTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
 
 import static java.nio.file.Files.readString;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
